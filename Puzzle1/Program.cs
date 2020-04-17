00be04288778514d3aa962ae2f372bb9dca92e66
Manoj M <manojr.329@gmail.com>
2020-04-17 14:25:41 +0530

    Uploaded

---
+++ Puzzle1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Logic1
+{
+    class Program
+    {
+
+        public static void logic1(int[] myNum, int snumber)
+        {
+            //var watch = new System.Diagnostics.Stopwatch();
+            //watch.Start();
+            Console.WriteLine("Puzzle Method 1");
+            Array.Sort(myNum);
+
+            int low = 0;
+            int high = myNum.Length - 1;
+
+            while (low < high)
+            {
+                if (myNum[low] + myNum[high] == snumber)
+                {                
+                    Console.WriteLine("Matche found at Index: " + low + " and " + high + "(" + myNum[low] + "+" + myNum[high] + ")\n");
+                    low++;
+
+                }
+                else if (myNum[low] + myNum[high] < snumber)
+                {
+                    low = low + 1;
+                }
+                else
+                {
+                    high = high - 1;
+                }
+            }
+            // watch.Stop();
+            //Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        public static void logic2(int[] myNum, int snumber)
+        {
+            Console.WriteLine("Puzzle Method 2");
+            for (int i = 0; i < myNum.Length; i++)
+            {
+                for (int j = i + 1; j < myNum.Length; j++)
+                {
+
+                    if (myNum[i] + myNum[j] == snumber)
+                    {                        
+                        Console.WriteLine("Match found at Index:" + i + " and " + j + "(" + myNum[i] + "+" + myNum[j] + ")\n");
+                    }
+
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Size of Array:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] myNum = new int[num];
+            Console.WriteLine("Enter the Sum Number that is to be Matched:");
+            int snumber = Convert.ToInt32(Console.ReadLine()); ;
+            Console.WriteLine("Enter Elements into Array:");
+            for (int a = 0; a < num; a++)
+            {
+                myNum[a] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+            logic1(myNum, snumber);
+            logic2(myNum, snumber);
+        }
+    }
+}
---
+++ PuzzlePattern/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace FactoryDesign
+{
+    /// The 'Product' Abstract Class  
+
+    public abstract class ArrayValues
+    {
+        public abstract string logic { get; }
+        public abstract int[] myNum { get; set; }
+        public abstract int snumber { get; set; }
+    }
+
+
+    /// A 'ConcreteProduct' class  
+
+    public class Method1 : ArrayValues
+    {
+        private  string _logic;
+        private int[] _myNum;
+        private int _snumber;
+        public Method1(int[] myNum, int snumber)
+        {
+            _myNum = myNum;
+            _snumber = snumber;
+            Console.WriteLine("Puzzle Method 1");
+            for (int i = 0; i < myNum.Length; i++)
+            {
+                for (int j = i + 1; j < myNum.Length; j++)
+                {
+
+                    if (myNum[i] + myNum[j] == snumber)
+                    {
+                        Console.WriteLine("Match found at Index:" + i + " and " + j + "(" + myNum[i] + "+" + myNum[j] + ")\n");
+                    }
+
+                }
+            }
+        }
+
+        public override string logic => throw new NotImplementedException();
+
+        public override int[] myNum { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int snumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+
+
+
+    /// The 'Creator' Abstract Class  
+
+    public abstract class ArrayFactory
+    {
+        public abstract ArrayValues GetArrayValues();
+    }
+
+    /// A 'ConcreteCreator' class  
+
+    public class Method1Factory : ArrayFactory
+    {
+        private string _logic;
+        private int[] _myNum;
+        private int _snumber;
+        public Method1Factory(int[] myNum, int snumber)
+        {
+            _myNum = myNum;
+            _snumber = snumber;
+        }
+
+        public override ArrayValues GetArrayValues()
+        {
+            return new Method1(_myNum, _snumber);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayFactory factory = null;
+            Console.WriteLine("Enter the Method you want to ues\n1. Method1\n2. Method2\n");
+            int method = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Size of Array:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] myNum = new int[num];
+            Console.WriteLine("Enter the Sum Number that is to be Matched:");
+            int snumber = Convert.ToInt32(Console.ReadLine()); ;
+            Console.WriteLine("Enter Elements into Array:");
+            for (int a = 0; a < num; a++)
+            {
+                myNum[a] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            switch (method)
+            {
+                case 1:
+                    factory = new Method1Factory(myNum, snumber);
+                    break;
+                /*case "method2":
+                    factory = new TitaniumFactory(100000, 500);
+                    break;*/
+                default:
+                    break;
+            }
+
+            /*arrayvalues arrayvalues = factory.Getarrayvalues();
+            Console.WriteLine("\nYour method details are below : \n");
+            Console.WriteLine("method Type: {0}\nCredit Limit: {1}\nAnnual Charge: {2}",
+                arrayvalues.logic, arrayvalues.[] myNum, arrayvalues.snumber);*/
+        }
+    }
+}
